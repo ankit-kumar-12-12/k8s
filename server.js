3b78c116bf2fd1e9e78c65d0a3c8be7e5fc24f30
Ankit Kumar <ankit.kumar.om108@gmail.com>
2024-04-22 23:36:47 +0530

    k8s

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+
+app.get("/api",(req,res)=>{
+    res.json({"users" : ["u1","u2","u3","u4"]})
+})
+
+app.listen(5000,()=>{
+
+    console.log("Server is listening at port 5000")
+}) 
+
+//to check http://localhost:5000/api
